3a1463ab8eabd3f58be0e38b5d3db1b38723f8c0
Antonio Tudela Botella <108870102+Tudelaa@users.noreply.github.com>
2022-11-03 16:22:05 +0100

    Add files via upload

---
+++ O365BasedLicense.ps1
@@ -0,0 +1,26 @@
+﻿
+# This script changes the registry key value called O365ProPlusRetail.DeviceBasedLicensing To configure device-based licensing if you cannot use
+# OfficeDeployment tool as is explained on next Microsoft link:
+# https://learn.microsoft.com/en-us/deployoffice/device-based-licensing
+
+# Written: Antonio Tudela 
+
+
+
+
+function O365_devicebasedlicense { 
+
+                  
+                    
+                    $registrypath = "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration" 
+                    $name = "O365ProPlusRetail.DeviceBasedLicensing"
+                    $value = "1"
+
+                    New-ItemProperty -Path $registrypath -Name $name -Value $value -Propertytype string -Force
+
+
+
+                    }
+
+
+O365_devicebasedlicense
